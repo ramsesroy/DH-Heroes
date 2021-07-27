54ee31611639ea1f661a5d81888cf19fec3a4820
ramsesroy <ramsesadorno@gmail.com>
2021-07-27 19:18:41 -0300

    edit

--- app.js
+++ app.js
@@ -3,30 +3,29 @@ const path = require('path');
 const app = express();
 const port = 3030;
 
-app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'))
+    res.sendFile(path.join(views, 'index.html'));
 });
 app.get('/babbage', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/babbage.html'))
+    res.sendFile(path.join(views, 'babbage.html'));
 });
 app.get('/berners-lee', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/berners-lee.html'))
+    res.sendFile(path.join(views, 'berners-lee.html'));
 });
 app.get('/clarke', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/clarke.html'))
+    res.sendFile(path.join(views, 'clarke.html'));
 });
 app.get('/hamilton', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/hamilton.html'))
+    res.sendFile(path.join(views, 'hamilton.html'));
 });
 app.get('/hopper', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/hopper.html'))
+    res.sendFile(path.join(views, 'hopper.html'));
 });
 app.get('/lovelace', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/lovelace.html'))
+    res.sendFile(path.join(views, 'lovelace.html'));
 });
 app.get('/turing', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/turing.html'))
+    res.sendFile(path.join(views, 'turing.html'));
 });
 
 
